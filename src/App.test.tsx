aa8016cb7e15e5c3e7675195416b86ec401d83af
Suryakant Devkar <surykant.devkar@wellsky.com>
2022-07-19 15:26:36 +0530

    Fixed the unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("home page text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Constructive IQ/i);
+  const linkElement = screen.getByText(/Homepage/i);
   expect(linkElement).toBeInTheDocument();
 });
