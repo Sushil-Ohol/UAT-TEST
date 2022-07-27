3af12a11c26dd66c3e7bd7bcd5dd769f83fa907b
varadmayee7798 <varad.mayee@nitorinfotech.com>
2022-07-27 12:50:28 +0530

    Added navbar

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,19 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("home page text", () => {
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn()
+    }))
+  });
   render(<App />);
   const linkElement = screen.getByText(/Homepage/i);
   expect(linkElement).toBeInTheDocument();
