15f444826d8002e05de66201da8b02d63266b912
Vaibhav-Bhosale7 <vaibhav-bhosale@nitorinfotech.com>
2022-08-03 14:12:08 +0530

    Navbar code updated as per latets verison of ANTD

---
+++ src/components/navbar/menus.tsx
@@ -0,0 +1,68 @@
+/* Project List Page Component */
+import { useState } from "react";
+import { UserOutlined } from "@ant-design/icons";
+import { Menu, MenuProps } from "antd";
+
+function Menus() {
+  const items: MenuProps["items"] = [
+    {
+      label: (
+        <a href="Project" rel="noopener noreferrer">
+          Project
+        </a>
+      ),
+      key: "1"
+    },
+    {
+      label: (
+        <a href="schedule" rel="noopener noreferrer">
+          Schedule
+        </a>
+      ),
+      key: "2"
+    },
+    {
+      label: (
+        <a href="specification" rel="noopener noreferrer">
+          Specification
+        </a>
+      ),
+      key: "3"
+    },
+    {
+      label: (
+        <a href="materials" rel="noopener noreferrer">
+          Material
+        </a>
+      ),
+      key: "4"
+    },
+    {
+      label: (
+        <a href="submittals" rel="noopener noreferrer">
+          Submittal
+        </a>
+      ),
+      key: "5"
+    },
+    { label: <UserOutlined className="UserOutlined" />, key: "6" }
+  ];
+
+  const [current, setCurrent] = useState("");
+
+  const onClick: MenuProps["onClick"] = (e) => {
+    setCurrent(e.key);
+  };
+
+  return (
+    <Menu
+      onClick={onClick}
+      selectedKeys={[current]}
+      items={items}
+      mode="horizontal"
+      style={{ float: "right", width: "100%" }}
+    />
+  );
+}
+
+export default Menus;
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,11 +1,9 @@
 /* Navigation Component */
-import { UserOutlined } from "@ant-design/icons";
-import { Menu, Col, Row, PageHeader } from "antd";
-import { Link } from "react-router-dom";
+import { Col, Row, PageHeader } from "antd";
+import Menus from "./menus";
 import "./navbar.css";
 
 function Nav() {
-  const style = { color: "black" };
   return (
     <Row className="Navbar">
       <Col span={2} offset={0}>
@@ -22,41 +20,7 @@ function Nav() {
         />
       </Col>
       <Col span={12} offset={2}>
-        <Menu mode="horizontal" style={{ float: "right", width: "100%" }}>
-          <Link to="project">
-            <Menu.Item key="keys2" style={style}>
-              Project
-            </Menu.Item>
-          </Link>
-          <Link to="Secification">
-            <Menu.Item key="keys3" style={style}>
-              Specification
-            </Menu.Item>
-          </Link>
-          <Link to="/floors">
-            <Menu.Item key="keys4" style={style}>
-              Floors
-            </Menu.Item>
-          </Link>
-          <Link to="/Schedule">
-            <Menu.Item key="keys5" style={style}>
-              Schedule
-            </Menu.Item>
-          </Link>
-          <Link to="/materials">
-            <Menu.Item key="keys6" style={style}>
-              Materials
-            </Menu.Item>
-          </Link>
-          <Link to="/submittals">
-            <Menu.Item key="keys7" style={style}>
-              Submittals
-            </Menu.Item>
-          </Link>
-          <Menu.Item key="keys7" style={style}>
-            <UserOutlined className="UserOutlined" />
-          </Menu.Item>
-        </Menu>
+        <Menus />
       </Col>
       <Col
         span={1}
@@ -66,4 +30,5 @@ function Nav() {
     </Row>
   );
 }
+
 export default Nav;
