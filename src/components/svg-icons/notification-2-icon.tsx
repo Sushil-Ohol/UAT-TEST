cda0925c7f17df425894dc89c4ddb75cef96cb52
Vaibhav-Bhosale7 <vaibhav.bhosale@nitorinfotech.com>
2022-08-29 11:54:35 +0530

    change svg icon name for notification1 and notification2

---
+++ src/components/svg-icons/notification-2-icon.tsx
@@ -0,0 +1,39 @@
+export default function Notification2Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+    >
+      <g
+        id="Notifications_Icon"
+        data-name="Notifications Icon"
+        transform="translate(-419 -206)"
+      >
+        <circle
+          id="Ellipse_5"
+          data-name="Ellipse 5"
+          cx="10"
+          cy="10"
+          r="10"
+          transform="translate(419 206)"
+          fill="#ff6a6a"
+        />
+        <text
+          id="_2"
+          data-name="2"
+          transform="translate(426 220)"
+          fill="#fff"
+          fontSize="11"
+          fontFamily="Inter-Medium, Inter"
+          fontWeight="500"
+        >
+          <tspan x="0" y="0">
+            2
+          </tspan>
+        </text>
+      </g>
+    </svg>
+  );
+}
