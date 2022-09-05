5762e60f00cb363ee04bbc8c4dae26a4795afcf5
varadmayee7798 <varad.mayee@nitorinfotech.com>
2022-09-05 15:01:08 +0530

    Add new column added currency type

--- src/pages/submittals-list/add-new-column/add-new-column.tsx
+++ src/pages/submittals-list/add-new-column/add-new-column.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/default-param-last */
 /* eslint-disable react/no-unstable-nested-components */
 import React, { useState } from "react";
 import { Button, DatePicker, Form, Input, Modal, Select } from "antd";
@@ -15,50 +16,60 @@ function AddNewColumn({ setNewColumnDataField, newColumnDataField }: any) {
   function NewDatePicker() {
     return <DatePicker />;
   }
+  function currencyFormatter(currency: number = 0, sign: string) {
+    const sansDec = currency.toFixed(2);
+    const formatted = sansDec.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return `${sign} ${formatted}`;
+  }
   const handleOk = () => {
-    setIsModalVisible(false);
-    const formValue = form.getFieldsValue();
-    if (formValue.inputType === "date") {
-      setNewColumnDataField([
-        ...newColumnDataField,
-        {
-          field: formValue.name,
-          headerName: formValue.name.toUpperCase(),
-          minWidth: 140,
-          cellEditor: NewDatePicker
-        }
-      ]);
-    } else if (formValue.inputType === "number") {
-      setNewColumnDataField([
-        ...newColumnDataField,
-        {
-          field: formValue.name,
-          headerName: formValue.name.toUpperCase(),
-          minWidth: 140,
-          cellEditor: NewDatePicker
-        }
-      ]);
-    } else if (formValue.inputType === "currency") {
-      setNewColumnDataField([
-        ...newColumnDataField,
-        {
-          field: formValue.name,
-          headerName: formValue.name.toUpperCase(),
-          minWidth: 140,
-          cellEditor: NewDatePicker
-        }
-      ]);
-    } else {
-      setNewColumnDataField([
-        ...newColumnDataField,
-        {
-          field: formValue.name,
-          headerName: formValue.name.toUpperCase(),
-          minWidth: 140
-        }
-      ]);
-    }
-    form.resetFields();
+    form.validateFields().then(() => {
+      setIsModalVisible(false);
+      const formValue = form.getFieldsValue();
+      if (formValue.inputType === "date") {
+        setNewColumnDataField([
+          ...newColumnDataField,
+          {
+            field: formValue.name,
+            headerName: formValue.name.toUpperCase(),
+            minWidth: 140,
+            cellEditor: NewDatePicker
+          }
+        ]);
+      } else if (formValue.inputType === "number") {
+        setNewColumnDataField([
+          ...newColumnDataField,
+          {
+            field: formValue.name,
+            headerName: formValue.name.toUpperCase(),
+            minWidth: 140,
+            cellEditor: () => <Input type="number" />
+          }
+        ]);
+      } else if (formValue.inputType === "currency") {
+        setNewColumnDataField([
+          ...newColumnDataField,
+          {
+            field: formValue.name,
+            headerName: formValue.name.toUpperCase(),
+            minWidth: 140,
+            editable: true,
+            filter: "agNumberColumnFilter",
+            valueFormatter: (params: any) =>
+              currencyFormatter(params.data.currency, "$")
+          }
+        ]);
+      } else {
+        setNewColumnDataField([
+          ...newColumnDataField,
+          {
+            field: formValue.name,
+            headerName: formValue.name.toUpperCase(),
+            minWidth: 140
+          }
+        ]);
+      }
+      form.resetFields();
+    });
   };
 
   const handleCancel = () => {
@@ -78,14 +89,21 @@ function AddNewColumn({ setNewColumnDataField, newColumnDataField }: any) {
         okText="Add"
         onCancel={handleCancel}
       >
-        <Form form={form} layout="vertical" autoComplete="off">
-          <Form.Item name="name" label="Name" className="add-new-column-label">
+        <Form form={form} layout="vertical" name="control-hooks" preserve>
+          <Form.Item
+            name="name"
+            label="Name"
+            className="add-new-column-label"
+            rules={[{ required: true, message: "Please input your name!" }]}
+            validateTrigger="onBlur"
+          >
             <Input name="name" className="add-new-column-input" />
           </Form.Item>
           <Form.Item
             className="add-new-column-label"
             name="inputType"
             label="Input type"
+            rules={[{ required: true, message: "Please select the type!" }]}
           >
             <Select
               className="add-new-column-input-type"
