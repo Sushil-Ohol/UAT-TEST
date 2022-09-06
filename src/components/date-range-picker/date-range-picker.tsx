78e458c65e22760beb43be8b4e3f213d9ad19d6e
Suryakant Dekvar <71318428+suryakantDNitor@users.noreply.github.com>
2022-09-06 12:11:04 +0530

    Update date-range-picker.tsx

--- src/components/date-range-picker/date-range-picker.tsx
+++ src/components/date-range-picker/date-range-picker.tsx
@@ -1,24 +1,10 @@
-import { Modal } from "antd";
+import { Modal, DatePicker, Button } from "antd";
 import "./date-range-picker.css";
-import { DateRangePicker } from "react-date-range";
-import { useState } from "react";
-import "react-date-range/dist/styles.css"; // main style file
-import "react-date-range/dist/theme/default.css"; // theme css file
+
+const { RangePicker } = DatePicker;
 
 function DateRangePickerModal(props: any) {
   const { title, isOpen, setIsOpen, setCustomDateRange, onOkClick } = props;
-
-  const [state, setState] = useState([
-    {
-      startDate: new Date(),
-      endDate: new Date()
-    }
-  ]);
-
-  const handleChange = (item: any) => {
-    setState([item.range1]);
-    setCustomDateRange(item.range1);
-  };
 
   return (
     <div>
@@ -26,23 +12,38 @@ function DateRangePickerModal(props: any) {
         title={title}
         visible={isOpen}
         className="custom-date-picker"
-        okText="Apply"
-        onOk={() => {
-          onOkClick();
-          setIsOpen(false);
-        }}
-        onCancel={() => {
-          setCustomDateRange();
-          setIsOpen(false);
-        }}
+        footer={[
+          <Button
+            key="back"
+            onClick={() => {
+              setCustomDateRange();
+              setIsOpen(false);
+            }}
+          >
+            Cancel
+          </Button>,
+          <Button
+            key="submit"
+            type="primary"
+            onClick={() => {
+              onOkClick();
+              setIsOpen(false);
+            }}
+          >
+            Apply
+          </Button>
+        ]}
       >
-        <DateRangePicker
-          onChange={(item: any) => handleChange(item)}
-          moveRangeOnFirstSelection={false}
-          months={2}
-          ranges={state}
-          direction="horizontal"
-          showDateDisplay={false}
+        <RangePicker
+          autoFocus
+          open
+          className="customRangePicker1"
+          format="DD-MM-YYYY"
+          onChange={(value) => setCustomDateRange(value)}
+          defaultOpen
+          getPopupContainer={(trigger) => {
+            return trigger;
+          }}
         />
       </Modal>
     </div>
