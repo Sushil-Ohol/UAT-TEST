dbd3c0a046edecb0ec4007e744f718a42fdabc9f
Suryakant Dekvar <71318428+suryakantDNitor@users.noreply.github.com>
2022-09-13 17:58:52 +0530

    Update index.tsx

--- src/pages/staging-zone/index.tsx
+++ src/pages/staging-zone/index.tsx
@@ -2,10 +2,12 @@ import { useAppDispatch } from "store";
 import { useSelector } from "react-redux";
 import { RootState } from "store/slices";
 import React from "react";
+import { Col, Row } from "antd";
 import { GetDiscussions } from "store/slices/staging-zone-slice";
 import Discussions from "./discussion-list";
 import DiscussionDetails from "./discussion-details";
 import DiscussionDocs from "./discussion-docs";
+import "./staging-zone.css";
 
 function StagingZone() {
   const dispatch = useAppDispatch();
@@ -22,11 +24,17 @@ function StagingZone() {
   }, []);
 
   return (
-    <div>
-      <Discussions />
-      <DiscussionDetails />
-      <DiscussionDocs />
-    </div>
+     <Row className="stagingZone">
+      <Col span={8} style={{ borderRight: "3px solid #0000001A" }}>
+        <Discussions className="stagingZoneCardContent" />
+      </Col>
+      <Col span={8} style={{ borderRight: "3px solid #0000001A" }}>
+        <DiscussionDetails className="stagingZoneCardContent" />
+      </Col>
+      <Col span={8}>
+        <DiscussionDocs className="stagingZoneCardContent" />
+      </Col>
+    </Row>
   );
 }
 
