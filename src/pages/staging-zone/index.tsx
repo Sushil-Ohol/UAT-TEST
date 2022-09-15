9bef96875c26ba2f50ced857df0e51a7bfec7ebd
Suryakant Dekvar <71318428+suryakantDNitor@users.noreply.github.com>
2022-09-15 19:07:48 +0530

    Added Resizable stazing zone panel (#137)
    * Update index.tsx
    
    * Update submittal-list.tsx
    
    * fixes liniting issues
    
    Co-authored-by: Suryakant Devkar <surykant.devkar@wellsky.com>

--- src/pages/staging-zone/index.tsx
+++ src/pages/staging-zone/index.tsx
@@ -1,10 +1,12 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useState } from "react";
 import Discussions from "./discussion-list";
 import DiscussionDetails from "./discussion-details";
 import DiscussionDocs from "./discussion-docs";
 import "./staging-zone.css";
 
-function StagingZone() {
+function StagingZone(props: any) {
+  const { onMouseDown } = props;
   const [discussionId, setDiscussionId] = useState("");
 
   React.useEffect(() => {}, []);
@@ -15,6 +17,20 @@ function StagingZone() {
 
   return (
     <div id="page-wrap">
+      <div
+        style={{
+          position: "absolute",
+          height: "5px",
+          padding: "0 4px 0",
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 100,
+          cursor: "ns-resize",
+          backgroundColor: "#f4f7f9"
+        }}
+        onMouseDown={onMouseDown}
+      />
       <div className="five-columns group">
         <Discussions
           className="col discussion"
