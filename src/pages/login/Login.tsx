b32b5f5ad576d9ddac22ec553585227e995557eb
Suryakant Dekvar <71318428+suryakantDNitor@users.noreply.github.com>
2022-09-17 23:50:34 +0530

    Create Login.tsx

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,98 @@
+import { Form, Input, Button, Typography, message as messages } from "antd";
+import FormItem from "antd/lib/form/FormItem";
+import { useState } from "react";
+import { isFulfilled, isRejected } from "@reduxjs/toolkit";
+import "./login.css";
+import { useHistory } from "react-router-dom";
+import { useAppDispatch } from "store";
+import { LoginRequest } from "models/user";
+import { login } from "store/slices/signIn";
+import { validationErrorMsg } from "../../constants";
+
+function Login() {
+  const [loader, setLoader] = useState(false);
+  const [form] = Form.useForm();
+  const dispatch = useAppDispatch();
+
+  // let navigate = useNavigate()
+  const history = useHistory();
+  // login button click
+  const onFinish = async (values: LoginRequest) => {
+    setLoader(true);
+    const actoinResult = await dispatch(
+      login({ email: values.email, password: values.password })
+    );
+    if (actoinResult && isFulfilled(actoinResult)) {
+      setLoader(false);
+      if (actoinResult.payload.success) {
+        messages.success(actoinResult.payload.message);
+        history.push("/project/new", { replace: true });
+      }
+    } else if (actoinResult && isRejected(actoinResult)) {
+      setLoader(false);
+      messages.error(actoinResult.payload.message);
+    }
+    form.resetFields();
+  };
+
+  const { Title } = Typography;
+
+  return (
+    <div className="login">
+      <div className="login-form">
+        <div className="login-logo">
+          <Title level={3}>Login</Title>
+        </div>
+        <Form
+          onFinish={onFinish}
+          form={form}
+          labelCol={{ span: 50 }}
+          wrapperCol={{ span: 30 }}
+          scrollToFirstError
+          layout="vertical"
+        >
+          <FormItem
+            name="email"
+            label="Email"
+            rules={[
+              {
+                required: true,
+                message: validationErrorMsg.onEmailIdFieldEmpty
+              },
+              {
+                type: "email",
+                message: validationErrorMsg.onEmailIdIsNotValid
+              }
+            ]}
+          >
+            <Input placeholder="Enter Email" />
+          </FormItem>
+          <FormItem
+            name="password"
+            label="Password"
+            className="password"
+            rules={[
+              {
+                required: true,
+                message: validationErrorMsg.onPasswordFieldEmpty
+              }
+            ]}
+          >
+            <Input.Password placeholder="Enter Password" />
+          </FormItem>
+          <FormItem>
+            <Button
+              htmlType="submit"
+              className="login-form-button"
+              loading={loader}
+            >
+              Login
+            </Button>
+          </FormItem>
+        </Form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
