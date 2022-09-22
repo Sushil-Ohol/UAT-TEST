2c83693eafc85c3121938a6968b9ce5dc95ff7ee
Suryakant Dekvar <71318428+suryakantDNitor@users.noreply.github.com>
2022-09-22 12:24:21 +0530

    Create clear-icon.tsx

---
+++ src/components/svg-icons/clear-icon.tsx
@@ -0,0 +1,18 @@
+export default function ClearIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="10"
+      height="10"
+      viewBox="0 0 10 10"
+    >
+      <path
+        id="_"
+        data-name="×"
+        d="M9.449,10.818.636,2.006,1.824.818l8.813,8.812Zm-7.625,0L.636,9.631,9.449.818l1.188,1.188Z"
+        transform="translate(-0.636 -0.818)"
+        fill="rgba(0,0,0,0.5)"
+      />
+    </svg>
+  );
+}
