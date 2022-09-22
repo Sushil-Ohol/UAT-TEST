e2ee7a57cdf14fee6753f73364660dc66a2bfb91
Suryakant Dekvar <71318428+suryakantDNitor@users.noreply.github.com>
2022-09-22 12:20:56 +0530

    Create search-dropdown.tsx

---
+++ src/components/submittal-details/search-dropdown.tsx
@@ -0,0 +1,56 @@
+import { Col, Row, Select } from "antd";
+import { SearchIcon } from "components/svg-icons";
+
+function SearchDropdown({
+  placeholder,
+  data,
+  onSelect
+}: {
+  placeholder: string;
+  data: any;
+  onSelect: Function;
+}) {
+  const { Option } = Select;
+  const onChange = (value: string) => {
+    onSelect(value);
+  };
+
+  return (
+    <Row style={{ backgroundColor: "#0000000D" }}>
+      <Col
+        span={4}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <SearchIcon />
+      </Col>
+      <Col span={20}>
+        <Select
+          style={{ width: "100%" }}
+          bordered={false}
+          showSearch
+          allowClear
+          placeholder={placeholder}
+          optionFilterProp="children"
+          onChange={onChange}
+          filterOption={(input, option) =>
+            (option!.children as unknown as string)
+              .toLowerCase()
+              .includes(input.toLowerCase())
+          }
+        >
+          {data.map((item: any) => (
+            <Option value={item.submittalId} key={item.submittalId}>
+              {item.submittal}
+            </Option>
+          ))}
+        </Select>
+      </Col>
+    </Row>
+  );
+}
+
+export default SearchDropdown;
