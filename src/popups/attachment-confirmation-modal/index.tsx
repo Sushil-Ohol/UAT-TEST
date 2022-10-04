04425e40e44292ebcf13a13cd09a57ad395969dc
Suryakant Dekvar <71318428+suryakantDNitor@users.noreply.github.com>
2022-10-04 13:03:41 +0530

    Update index.tsx

--- src/popups/attachment-confirmation-modal/index.tsx
+++ src/popups/attachment-confirmation-modal/index.tsx
@@ -1,8 +1,20 @@
 import { Input, Modal } from "antd";
+import { AttachmentConfirmationMessages } from "constants/index";
 import "./attachment-confirmation.css";
 
-function ConfirmAttachModal(props: any) {
-  const { isModalOpen, setIsModalOpen, handleOk } = props;
+interface AttachmentConfirmationModalProps {
+  isModalOpen: boolean;
+  setIsModalOpen: Function;
+  handleOk: any;
+  type: string;
+}
+
+function AttachmentConfirmationModal(props: AttachmentConfirmationModalProps) {
+  const { type, isModalOpen, setIsModalOpen, handleOk } = props;
+  const onOkClick = () => {
+    console.log("okk");
+    handleOk();
+  };
   return (
     <Modal
       className="attach-document"
@@ -10,43 +22,20 @@ function ConfirmAttachModal(props: any) {
       visible={isModalOpen}
       onCancel={() => setIsModalOpen(false)}
       okText="Attach"
-      onOk={handleOk}
+      onOk={onOkClick}
     >
-      <p
-        style={{
-          font: "18px/21px Inter",
-          letterSpacing: "0",
-          color: "#000000FA",
-          fontWeight: 600
-        }}
-      >
-        Do you want to attach this document into this submittal?
+      <p className="attachment-heading">
+        {AttachmentConfirmationMessages.heading.replace("{type}", type)}
       </p>
-      <p
-        style={{
-          font: "16px/24px Inter",
-          color: "#000000",
-          letterSpacing: "0",
-          fontWeight: 500
-        }}
-      >
-        This will give access to everyone in this submittal including external
-        users outside of your org.
+      <p className="attachment-sub-heading">
+        {AttachmentConfirmationMessages.subHeading.replace("{type}", type)}
       </p>
       <p>
         <Input type="checkbox" style={{ width: "auto" }} />
-        <span
-          style={{
-            font: "font:14px/17px Inter",
-            color: "#0000007F",
-            marginLeft: "5px"
-          }}
-        >
-          Remove all annotations
-        </span>
+        <span className="annotation-text">Remove all annotations</span>
       </p>
     </Modal>
   );
 }
 
-export default ConfirmAttachModal;
+export default AttachmentConfirmationModal;
