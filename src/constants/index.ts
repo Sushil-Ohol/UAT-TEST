d0295f530613ee1b1ac6b188afb0518fd6eff34c
Suryakant Dekvar <71318428+suryakantDNitor@users.noreply.github.com>
2022-10-04 12:53:05 +0530

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -17,6 +17,19 @@ export const ErrorMessages = {
   PasswordRequired: "Please Enter Password"
 };
 
+export const AttachmentConfirmationMessages: any = {
+  submittalDetail: {
+    heading: "Do you want to attach this document into this submittal details?",
+    subHeading:
+      "This will give access to everyone in this submittal including external users outside of your org."
+  },
+  material: {
+    heading: "Do you want to attach this document into this material?",
+    subHeading:
+      "This will give access to everyone in this material including external users outside of your org."
+  }
+};
+
 export const steps = [
   {
     title: "Details",
